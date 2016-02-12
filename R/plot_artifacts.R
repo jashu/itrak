53dd809f73f6f53d8814615f8a09b6d2aafcec04
Jason Shumake <shumake@utexas.edu>
2016-02-12 17:09:12 -0600

    Add ability to plot multiple trials as facets

--- R/plot_artifacts.R
+++ R/plot_artifacts.R
@@ -9,32 +9,46 @@
 #' going undetected, try decreasing the \code{max_velocity} setting. If there
 #' are too many false positives, try increaasing it.
 #'
-#' @param time Numeric vector corresponding to time. Will be plotted on the x-
-#' axis. Must be the same length as the \code{pupil} and \code{artifact}
-#' vectors.
+#' @param data Data frame containing time series.
 #'
-#' @param pupil Numeric vector containing the time series of pupil measurements.
-#' Will be plotted on the y-axis. Must be the same length as the \code{time}
-#' and \code{artifact} vectors.
+#' @param time Name of variable that gives time units. Will be plotted on the x-
+#' axis.
 #'
-#' @param artifacts Logical vector that indexes which observations in the
-#' \code{pupil} time series are artifacts. See \code{\link{get_artifacts}}.
+#' @param pupil Name of variable containing the time series of pupil measures.
+#' Will be plotted on the y-axis.
 #'
-#' @param data Data frame to use (optional).
+#' @param artifacts Name of variable containing logical vector that indexes
+#' which observations in the \code{pupil} time series are artifacts.
+#' See \code{\link{get_artifacts}}.
 #'
-#' @param ... optional arguments to \code{\link[ggplot2]{qplot}},
-#' e.g., "main", "ylim", etc.
+#' @param trial If the data contains multiple time series, name of the variable
+#'  that identifies each time series. Will be plotted using \code{facet_wrap}.
 #'
 #' @export
 
-plot_artifacts <- function(time, pupil, artifacts, data = NULL, ...){
+plot_artifacts <- function(data, time, pupil, artifacts, trial = NULL){
   arguments <- as.list(match.call())
   if(!is.null(data)){
     time = eval(arguments$time,data)
     pupil = eval(arguments$pupil,data)
     artifacts = eval(arguments$artifacts,data)
+    trial = eval(arguments$trial, data)
   }
-  ggplot2::qplot(time, pupil, geom = "line", ...) +
-    ggplot2::geom_vline(xintercept = time[artifacts], color = "red",
-                        linetype = "dotted") + theme_bw()
+  if(is.null(trial)){
+    trial <- rep(1, length(time))
+  }
+  pupil_data <- data_frame(time = time,
+                           pupil = pupil,
+                           artifacts = artifacts,
+                           trial = trial)
+  artifacts <- pupil_data %>% filter(artifacts)
+  mean_pupil <- mean(pupil_data$pupil, na.rm = T)
+  limits <- c(.5*mean_pupil, 1.5*mean_pupil)
+  ggplot() +
+    geom_line(aes(time, pupil), pupil_data) +
+    geom_vline(aes(xintercept = time), artifacts,
+               color = "red", linetype = "dotted") +
+    scale_y_continuous(limits = limits) +
+    facet_wrap(~trial) +
+    theme_bw()
 }

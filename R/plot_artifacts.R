005eb0631e51317f7c955ad1b13e847c8f0fce06
jashu <shumake@utexas.edu>
2016-02-14 18:10:15 -0600

    Change parameter name from pupil to measure to be more generic

--- R/plot_artifacts.R
+++ R/plot_artifacts.R
@@ -1,10 +1,10 @@
-#' Plot artifacts
+#' Plot Artifacts
 #'
-#' \code{plot_artifacts} returns a plot of a single time series with
+#' \code{plot_artifacts} returns a plot of time series with
 #' artifact intervals marked in red with vertical dotted lines.
 #'
 #' Use this plotting function to evaluate the performance of the
-#' \code{\link{get_artifacts}} function on individual time series and the
+#' \code{\link{get_artifacts}} function on one or more time series and the
 #' impact of changes to its threshold settings. If many artifacts are
 #' going undetected, try decreasing the \code{max_velocity} setting. If there
 #' are too many false positives, try increaasing it.
@@ -14,11 +14,11 @@
 #' @param time Name of variable that gives time units. Will be plotted on the x-
 #' axis.
 #'
-#' @param pupil Name of variable containing the time series of pupil measures.
+#' @param measure Name of variable containing the time series of measures.
 #' Will be plotted on the y-axis.
 #'
 #' @param artifacts Name of variable containing logical vector that indexes
-#' which observations in the \code{pupil} time series are artifacts.
+#' which observations in the \code{measure} time series are artifacts.
 #' See \code{\link{get_artifacts}}.
 #'
 #' @param trial If the data contains multiple time series, name of the variable
@@ -26,26 +26,26 @@
 #'
 #' @export
 
-plot_artifacts <- function(data, time, pupil, artifacts, trial = NULL){
+plot_artifacts <- function(data, time, measure, artifacts, trial = NULL){
   arguments <- as.list(match.call())
   if(!is.null(data)){
     time = eval(arguments$time,data)
-    pupil = eval(arguments$pupil,data)
+    measure = eval(arguments$measure,data)
     artifacts = eval(arguments$artifacts,data)
     trial = eval(arguments$trial, data)
   }
   if(is.null(trial)){
     trial <- rep(1, length(time))
   }
-  pupil_data <- data_frame(time = time,
-                           pupil = pupil,
+  measure_data <- data_frame(time = time,
+                           measure = measure,
                            artifacts = artifacts,
                            trial = trial)
-  artifacts <- pupil_data %>% filter(artifacts)
-  mean_pupil <- mean(pupil_data$pupil, na.rm = T)
-  limits <- c(.5*mean_pupil, 1.5*mean_pupil)
+  artifacts <- measure_data %>% filter(artifacts)
+  mean_measure <- mean(measure_data$measure, na.rm = T)
+  limits <- c(.5*mean_measure, 1.5*mean_measure)
   ggplot() +
-    geom_line(aes(time, pupil), pupil_data) +
+    geom_line(aes(time, measure), measure_data) +
     geom_vline(aes(xintercept = time), artifacts,
                color = "red", linetype = "dotted") +
     scale_y_continuous(limits = limits) +

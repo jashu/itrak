a9ee8821309458e5da011f94c6bd76ceab1abff5
Jason Shumake <shumake@utexas.edu>
2016-08-09 09:47:56 -0500

    Add ability to flag out-of-range periods in blue.

--- R/plot_artifacts.R
+++ R/plot_artifacts.R
@@ -1,13 +1,14 @@
 #' Plot Artifacts
 #'
-#' \code{plot_artifacts} returns a plot of time series with
-#' artifact intervals marked in red with vertical dotted lines.
+#' \code{plot_artifacts} returns a plot of time series with artifact intervals
+#' marked in red with dotted lines. Extreme values are marked with blue dotted
+#' lines.
 #'
 #' Use this plotting function to evaluate the performance of the
-#' \code{\link{get_artifacts}} function on one or more time series and the
-#' impact of changes to its threshold settings. If many artifacts are
-#' going undetected, try decreasing the \code{max_velocity} setting. If there
-#' are too many false positives, try increaasing it.
+#' \code{\link{get_artifacts}} and optionally \code{\link{get_oor}} functions on
+#' one or more time series. If many artifacts are going undetected, try
+#' decreasing the \code{max_velocity} setting. If there are too many false
+#' positives, try increaasing it.
 #'
 #' Plotting is performed using \code{\link[ggplot2]{ggplot}} and you can modify
 #' the aesthetics of the plot by appending layers using the \code{+} operator.
@@ -26,18 +27,27 @@
 #' which observations in the \code{measure} time series are artifacts.
 #' See \code{\link{get_artifacts}}.
 #'
+#' @param oor Optional name of variable containing logical vector that indexes
+#' which observations in the \code{measure} time series are out of range.
+#' See \code{\link{get_oor}}.
+#'
 #' @param trial If the data contains multiple time series, name of the variable
 #'  that identifies each time series. Will be plotted using \code{facet_wrap}.
 #'
 #' @export
 
-plot_artifacts <- function(data, time, measure, artifacts, trial = NULL){
+plot_artifacts <- function(data, time, measure, artifacts, oor = NULL,
+                           trial = NULL){
   arguments <- as.list(match.call())
   if(!is.null(data)){
     time <- eval(arguments$time,data)
     measure <- eval(arguments$measure,data)
-    artifacts <- eval(arguments$artifacts,data)
+    artifacts <- eval(arguments$artifacts, data)
+    oor <-eval(arguments$oor, data)
     trial <- eval(arguments$trial, data)
+  }
+  if(is.null(oor)){
+    oor <- rep(FALSE, length(time))
   }
   if(is.null(trial)){
     trial <- rep(1, length(time))
@@ -48,12 +58,16 @@ plot_artifacts <- function(data, time, measure, artifacts, trial = NULL){
   measure_data <- data_frame(time = time,
                            measure = measure,
                            artifacts = artifacts,
+                           oor = oor,
                            trial = trial)
   artifacts <- measure_data %>% filter(artifacts)
+  oor <- measure_data %>% filter(oor)
   ggplot() +
     geom_line(aes(time, measure), measure_data) +
     geom_vline(aes(xintercept = time), artifacts,
                color = "red", linetype = "dotted") +
+    geom_vline(aes(xintercept = time), oor,
+               color = "blue", linetype = "dotted") +
     scale_y_continuous(limits = c(ymin, ymax)) +
     facet_wrap(~trial) +
     ylab("") +

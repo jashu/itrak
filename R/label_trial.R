ad15efe676625d38fcb4a43f87d5d67c6d23a4ac
Jason Shumake <shumake@utexas.edu>
2016-11-22 18:05:51 -0600

    Modify to ignore invalid trials (missing event marker).

--- R/label_trial.R
+++ R/label_trial.R
@@ -22,18 +22,43 @@
 #' a key (in which case it will be faster to label the sequence of keys and
 #' then \code{\link[dplyr]{join}} this value to your time-series data).
 #'
+#' Optionally, an event-onset marker can be included that indicates that a time
+#' series belongs to a valid trial. If this onset marker is not detected for a
+#' time series, then \code{label_trial} will not include this series in the
+#' trial count and will return a label of \code{NA}. This can then be used as a
+#' flag for removing extraneous measurements, such as pupil readings taken
+#' during a drift check or calibration procedure.
+#'
+#' @return An integer vector of trial numbers reflecting the temporal order of
+#' multiple time series.
+#'
 #' @param sample Index number for the time series that resets with the start of
 #'  a new trial.
 #'
+#' @param marker Vector containing markers of trial events.
+#'
+#' @param event Label in \code{marker} that is uniquely associated with valid
+#' trials. If this is a character string, it must be enclosed in quotation
+#' marks.
+#'
 #' @export
 
-label_trial <- function(sample){
+label_trial <- function(sample, marker = NULL, event = NULL){
   trial <- vector("integer", length(sample))
   trial_counter <- 1
   trial[1] <- 1
   for(i in 2:length(sample)){
-    if(sample[i] < sample[i-1]) trial_counter <- trial_counter + 1
+    if(sample[i] < sample[i-1]){
+      if(!is.null(marker) && !event %in% marker[trial == trial_counter]){
+        trial[trial == trial_counter] <- NA_integer_
+      } else {
+        trial_counter <- trial_counter + 1
+      }
+    }
     trial[i] <- trial_counter
   }
-  return(trial)
+  if(!is.null(marker) && !event %in% marker[trial == trial_counter]){
+    trial[trial == trial_counter] <- NA_integer_
+  }
+  trial
 }

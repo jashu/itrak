d32853f7b19d6e1970a863b67e19f8b6adafe22f
jashu <shumake@utexas.edu>
2017-02-04 08:40:58 -0600

    Roxygen update

--- R/relabel_trial.R
+++ R/relabel_trial.R
@@ -1,3 +1,17 @@
+#' Relabel trial numbers
+#'
+#' Assign invalid trials and renumber remaining trials as a consecutive sequence
+#' from 1 to the total number of trials.
+#'
+#'
+# Note that the trial number labels in this file are counting drift-correct
+# trials. This will pose a problem when you attempt to join this file
+# to the artifact counts, which excluded drift-check trials. I added a
+# function `relabel_trial` to `itrak` that will renumber the trials,
+# assigning missing values to invalid trials. In this case, we label as
+# valid a trial that is not missing data for the `lstim` field.
+
+
 #' @export
 relabel_trial <- function(trial, valid){
   if(any(diff(trial) < 0))
